dbe97d54c0aeecb4f7cfb61aac3180e66d1c914d
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-12 19:28:31 -0500

    Add files via upload

---
+++ Methods.java/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+   
+    public static String greet() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+        String message = greet();  
+        System.out.println(message); 
+    }
+}
