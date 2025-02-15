b2b8b4e43e7475f4b113a56266c8861213a252cb
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-14 22:31:31 -0500

    Add files via upload

---
+++ DataTypes.java/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String[] args) {
+       
+        int age = 25;
+        double salary = 5000.50;
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+       
+        String message = "Hello, Java!";
+
+      
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Message: " + message);
+    }
+}
