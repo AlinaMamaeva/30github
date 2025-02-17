42b082ec57f2c3abf9feb05992fbd01b2bc180f9
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-16 19:39:15 -0500

    Add files via upload

---
+++ Replace.java/Replace.java
@@ -0,0 +1,9 @@
+public class Replace {
+    public static void main(String[] args) {
+
+   String str5 = "Sillicon Valley";
+        String str6 = str5.replace('l', 'o');
+        System.out.println(str6);
+
+    }
+}
