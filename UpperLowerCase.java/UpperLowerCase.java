65a121212a151dcd995b266ac438882b9ee70b46
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-17 21:36:09 -0500

    Add files via upload

---
+++ UpperLowerCase.java/UpperLowerCase.java
@@ -0,0 +1,9 @@
+public class UpperLowerCase {
+    public static void main(String[] args) {
+        
+
+        String str3 = "Spring Role";
+        String str4 = str3.toLowerCase();
+        System.out.println(str4);
+    }
+}
