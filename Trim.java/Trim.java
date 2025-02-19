bbf314b4581fc8ede74b3023d459d1174d72dd57
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-18 20:35:53 -0500

    Add files via upload

---
+++ Trim.java/Trim.java
@@ -0,0 +1,9 @@
+public class Trim {
+    public static void main(String[] args) {
+
+        String str = " Hello SuperStars";
+        String str2 = str.trim();
+
+        System.out.println(str2);
+}   
+ }
