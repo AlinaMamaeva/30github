26cb4e639eafa5c2220f3904da1401574453facd
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-19 21:37:52 -0500

    Add files via upload

---
+++ Length.java/Length.java
@@ -0,0 +1,10 @@
+public class Lenght {
+    public static void main(String[] args) {
+
+        String str ="alina";
+
+       int strLength =  str.length();
+
+        System.out.println(strLength);
+}
+   }
