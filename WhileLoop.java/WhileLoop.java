f2129a0edd181cbd254faeda7bae25a1f56a9b75
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-20 22:00:05 -0500

    Add files via upload

---
+++ WhileLoop.java/WhileLoop.java
@@ -0,0 +1,19 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int counter = 1;
+        while (counter <= 5) {
+            System.out.println("Superstar");
+            counter++;
+
+
+        }
+        int num = 1;
+        while (num <= 20){
+            System.out.println("Love java");
+            num++;
+        }
+
+
+    }
+}
