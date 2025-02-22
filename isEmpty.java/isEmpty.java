2451ffc9bf0e2733738dda3b86a603d369d44671
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-21 21:12:44 -0500

    Add files via upload

---
+++ isEmpty.java/isEmpty.java
@@ -0,0 +1,14 @@
+public class isEmpty {
+    public static void main(String[] args) {
+
+        String str = "I love java, it will take me to Mars";
+
+     
+      String str2 = "";
+       boolean isStrEmpty = str.isEmpty();
+       System.out.println(isStrEmpty);
+
+       boolean isStr2Empty = str2.isEmpty();
+        System.out.println(isStr2Empty);
+    }
+}
