785054d4b3da4b09699aa626843d8a570c4dd927
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-22 21:52:53 -0500

    Add files via upload

---
+++ Loop.java/Loop.java
@@ -0,0 +1,17 @@
+public class Loop {
+    public static void main(String[] args) {
+       
+       
+
+
+        int counter = 1;
+        int sum = 0;
+
+        while(counter <= 5){
+            sum+=counter;
+            System.out.println(sum);
+            counter++;
+
+        }
+    }
+}
