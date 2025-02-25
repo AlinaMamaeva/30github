37daddac880868cc7e49dd5dc59eaea864243a50
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-24 22:27:55 -0500

    Add files via upload

---
+++ index.java/index.java
@@ -0,0 +1,7 @@
+public class index {
+    public static void main(String[] args) {
+        String str = "Hello superstars!";
+        int indexOfS = str.indexOf("superstars");
+        System.out.println(indexOfS);
+ } 
+}
