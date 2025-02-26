dee895409757b2c31e2221efd41831621dbb1fc9
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-02-25 21:39:07 -0500

    Add files via upload

---
+++ StartWithEndWith.java/StartWithEndWith.java
@@ -0,0 +1,10 @@
+public class StartWithEndWith {
+    public static void main(String[] args) {
+
+    
+    String str3 = "Good weather";
+      boolean d3 = str3.endsWith("Good");
+        System.out.println(d3);
+
+} 
+  }
