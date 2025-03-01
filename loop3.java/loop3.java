7cf9cc142f79cc936f25b149a0c83dd8afb2c60a
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-03-01 13:31:28 -0700

    Add files via upload

---
+++ loop3.java/loop3.java
@@ -0,0 +1,19 @@
+public class loop3 {
+    public static void main(String[] args) {
+
+        int counter = 1;
+        while (counter <= 5) {
+            System.out.println("Superstar");
+            counter++;
+
+
+        }
+        int num = 1;
+        while (num <= 20){
+            System.out.println("Love java");
+            num++;
+        }
+
+
+    }
+}
