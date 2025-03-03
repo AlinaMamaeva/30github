6004e0ad4a43d6134720e9fb9df49d27d884a93c
Alina <141278918+AlinaMamaeva@users.noreply.github.com>
2025-03-03 13:07:30 -0700

    Add files via upload

---
+++ loop5.java/loop5.java
@@ -0,0 +1,31 @@
+public class loop5 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+
+
+        System.out.println("===============================");
+
+        int counter = 1; // starting from 1;
+        while (counter <= 5){
+            System.out.println(counter);
+
+            counter++;
+
+        }
+        System.out.println("===============");
+        int counter1 = 0; // Start point
+        while(counter1 <= 9){
+            System.out.println(counter1);
+            counter1++; // end point
+
+
+
+        }
+
+
+    }
+}
